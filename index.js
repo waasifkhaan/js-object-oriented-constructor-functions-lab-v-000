d191f0cc535b88444566f6a1358a7936db075327
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 00:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function Scooter(year,color,model) {
-  this.year = year 
-  this.color = color 
-  this.model = model 
+  this.year = year
+  this.color = color
+  this.model = model
 }
 
 function Driver(name, age , experience) {
-  this.name = name 
-  this.age = age 
-  this.experience = experience 
+  this.name = name
+  this.age = age
+  this.experience = experience
 }
 
 function PickupLocation(address,city) {
-  this.address = address 
-  this.city = city 
-  
+  this.address = address
+  this.city = city
+
 }
